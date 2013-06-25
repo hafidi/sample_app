87c9c89bf5a32d2d57aac1776c78be357654da45
Yassine Hafidi <yassine.hafidi@die-sw-factory.de>
2013-06-25 11:51:30 +0200

    Compile Assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module SampleApp
   class Application < Rails::Application
 
     config.serve_static_assets = true
-    
+    config.assets.compile = true
+    # Heroku also requires this to be false
+    config.assets.initialize_on_precompile=false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
